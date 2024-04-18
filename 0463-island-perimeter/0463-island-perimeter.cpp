cb279c7a5f85d98d027ee65f18e32d651ce6490c
Arshit Chaurasia <118971735+arshitcc@users.noreply.github.com>
2024-04-18 23:49:33 +0530

    Time: 166 ms (5.04%), Space: 131.8 MB (5.12%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    set<pair<int,int>> visited;
+    int perimtr = 0;
+    
+    void dfs(int i, int j, vector<vector<int>> &grid){
+        
+        if(i<0 or j<0 or i==grid.size() or j==grid[0].size()) return;
+        if(visited.count({i,j}) == 1) return;
+        
+        visited.insert({i,j});
+        perimtr += 4;
+        
+        if(j+1 <grid[0].size() and  grid[i][j+1] == 1){
+            perimtr --;
+            dfs(i,j+1,grid);
+        }
+        if(j-1>= 0 and grid[i][j-1] == 1){
+            perimtr --;
+            dfs(i,j-1,grid);
+        }
+        if(i+1 <grid.size() and  grid[i+1][j] == 1){
+            perimtr --;
+            dfs(i+1,j,grid);
+        }
+        if(i-1>=0 and grid[i-1][j] == 1){
+            perimtr --;
+            dfs(i-1,j,grid);
+        }
+    }
+    
+    int islandPerimeter(vector<vector<int>>& grid) {
+    
+        for(int i=0; i<grid.size(); i++){
+            bool got = false;
+            for(int j=0; j<grid[0].size(); j++){
+                if(grid[i][j] == 1 and visited.count({i,j}) == 0){
+                    dfs(i,j,grid);
+                    got = true;
+                    break;
+                }
+            }
+            if(got) break;
+        }
+        
+        return perimtr;
+    }
+};
