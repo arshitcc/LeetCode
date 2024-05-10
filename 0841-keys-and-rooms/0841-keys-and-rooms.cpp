17bb68fd8b6fe38b943c2a6181318532531d1aa3
Arshit Chaurasia <118971735+arshitcc@users.noreply.github.com>
2024-05-10 15:55:40 +0530

    Time: 6 ms (69.55%), Space: 14.2 MB (17.15%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    
+    // DFS : 
+    unordered_set<int> visited;
+    
+    void collectKeys(int curr, vector<vector<int>> &rooms){
+        
+        visited.insert(curr);
+        for(auto neighbour : rooms[curr]){
+            if(visited.count(neighbour) == 0){
+                visited.insert(neighbour);
+                collectKeys(neighbour,rooms);
+            }
+        }
+        
+    }
+    
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        
+        // DFS : 
+        visited.insert(0);
+        collectKeys(0, rooms);
+        
+        
+//         // BFS : 
+        
+//         unordered_set<int> visited;
+//         visited.insert(0);
+        
+//         queue<int> q;
+//         q.push(0);
+        
+//         while(q.size()){
+//             int curr = q.front(); q.pop();
+            
+//             visited.insert(curr);
+            
+//             for(int neighbour : rooms[curr]){
+//                 if(visited.count(neighbour) == 0){ // if not visited then visit.
+//                     visited.insert(neighbour);
+//                     q.push(neighbour);
+//                 }
+//             }
+            
+//         }
+        
+        return visited.size()==rooms.size();
+    }
+};
