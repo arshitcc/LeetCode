28e0305a95f5f5f43e57095cf9ceb802644d5f71
Arshit Chaurasia <118971735+arshitcc@users.noreply.github.com>
2024-05-12 03:12:22 +0530

    Time: 24 ms (74.23%), Space: 26.3 MB (68.63%) - LeetHub

---
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        
+        double minCost = DBL_MAX;
+        
+        int n = quality.size();
+        vector<pair<double,int>> pay;
+        
+        for(int i=0; i<n; i++){
+            double min_ratio = (double)wage[i]/quality[i];
+            pay.push_back({min_ratio, quality[i]});
+        }
+        
+        sort(pay.begin(), pay.end());
+        
+        double qualitySum = 0;
+        double minRatio = 0;
+        priority_queue<int> pq;
+        
+        for(int i=0; i<k; i++){
+            minRatio = max(minRatio, pay[i].first);
+            qualitySum += pay[i].second;
+            pq.push(pay[i].second);
+        }
+        
+        minCost = min(minCost, minRatio*qualitySum);
+        
+        for(int i=k; i<n; i++){
+            
+            // pay[i].first = new Fixed worker according to which pay will now be given
+            qualitySum -= pq.top(); pq.pop();
+            qualitySum += pay[i].second;
+            pq.push(pay[i].second);
+            
+            minCost = min(minCost, pay[i].first*qualitySum);
+        }
+        
+        return minCost;   
+    }
+};
