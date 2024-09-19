dfc6761e3430be76a2e404deae6e5e9a5ff5b75f
Arshit Chaurasia <118971735+arshitcc@users.noreply.github.com>
2024-09-19 17:31:08 +0530

    Time: 0 ms (100%), Space: 13.8 MB (32.27%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -23,6 +23,24 @@ public:
         return res;
     }
     vector<int> diffWaysToCompute(string s) {
-        return helper(s);
+        vector<int> res;
+
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '+' or s[i] == '-' or s[i] == '*'){
+                vector<int> leftop = diffWaysToCompute(s.substr(0,i));
+                vector<int> rightop = diffWaysToCompute(s.substr(i+1));
+
+                for(auto x : leftop){
+                    for(auto y : rightop){
+                        if(s[i]=='+') res.push_back(x+y);
+                        else if(s[i] == '-') res.push_back(x-y);
+                        else if(s[i] == '*') res.push_back(x*y);
+                    }
+                }
+            }
+        }
+
+        if(!res.size()) res.push_back(stoi(s));
+        return res;
     }
 };
