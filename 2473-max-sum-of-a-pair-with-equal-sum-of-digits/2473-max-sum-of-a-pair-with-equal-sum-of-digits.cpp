bd637d029f2477f84cba77ee24892acf9fc0a607
Arshit Chaurasia <118971735+arshitcc@users.noreply.github.com>
2025-02-13 05:36:28 +0530

    Time: 7 ms (94.68%), Space: 63.6 MB (95.59%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,44 +1,27 @@
 class Solution {
-private:
-    // Helper function to compute the sum of digits of a number
-    int calculateDigitSum(int num) {
-        int digitSum = 0;
-        while (num > 0) {
-            digitSum += num % 10;
-            num /= 10;
+public:
+    int getSum(int n) {
+        int res = 0;
+        while (n) {
+            res += (n % 10);
+            n /= 10;
         }
-        return digitSum;
+        return res;
     }
 
-public:
     int maximumSum(vector<int>& nums) {
-        // Vector to store a min heap for each possible digit sum (0 to 81)
-        vector<priority_queue<int, vector<int>, greater<int>>> digitSumGroups(
-            82);
 
-        int maxPairSum = -1;
+        vector<int>v(82,-1);
+        int maxi = -1;
 
-        // Group numbers by their digit sums, maintaining heap size of 2
-        for (int number : nums) {
-            int digitSum = calculateDigitSum(number);
-            digitSumGroups[digitSum].push(number);
-
-            // Keep only the top 2 largest numbers in the heap
-            if (digitSumGroups[digitSum].size() > 2) {
-                digitSumGroups[digitSum].pop();  // Remove the smallest element
+        for (auto num : nums) {
+            int s = getSum(num);
+            if(v[s]==-1) v[s]=num;
+            else{
+                maxi=max(maxi,v[s]+num);
+                v[s]=max(v[s],num);
             }
         }
-
-        // Traverse the vector to find the maximum pair sum for each group
-        for (auto& minHeap : digitSumGroups) {
-            if (minHeap.size() == 2) {
-                int first = minHeap.top();
-                minHeap.pop();
-                int second = minHeap.top();
-                maxPairSum = max(maxPairSum, first + second);
-            }
-        }
-
-        return maxPairSum;
+        return maxi;
     }
 };
